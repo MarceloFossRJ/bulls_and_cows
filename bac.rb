6aa42d8e7c6fd5069d372a9b94981883cd02e6b9
marcelofossrj <marcelo.foss.rj@gmail.com>
2017-11-29 17:04:32 -0200

    Secret word chooser prompt

--- bac.rb
+++ bac.rb
@@ -4,6 +4,9 @@ module BullsAndCows
       welcome
       instructions
       choose_to_play
+      choose_word
+      puts "You have chosen the word:" + @chosen_word
+      puts "---------------------------------"
     end
 
     def welcome
@@ -17,6 +20,7 @@ module BullsAndCows
       puts " "
       puts "First you have to choose a four-letter word."
       puts "The word to be guessed should not have repeating letters."
+      puts "Only letters are allowed!"
       puts " "
       puts "The computer then will guess the word."
       puts " "
@@ -32,7 +36,7 @@ module BullsAndCows
 
     def choose_to_play_input_validation(response)
       if response == "y"
-        puts "ok"
+        return
       elsif response == "n"
         quit_game
       else
@@ -44,6 +48,36 @@ module BullsAndCows
     def quit_game
       puts "Thank you, see you next time!"
     end
+
+    def choose_word
+      puts " "
+      puts "Choose now a four-letter word, with non repeting letters:"
+
+      input = gets.chomp.downcase
+      if choose_word_input_validation(input)
+        @chosen_word = input
+      else
+        input.clear
+        choose_word
+      end
+    end
+
+    def choose_word_input_validation(response)
+      if response.length != 4
+        puts "Invalid input: wrong length."
+        return false
+      end
+      if !response.match(/^[[:alpha:]]+$/)
+        puts "Invalid input: only letters accepted."
+        return false
+      end
+      if !(response !~ /([a-z]).*\1/)
+        puts "Invalid input: letters cannot be reapeted."
+        return false
+      end
+      return true
+    end
+
   end
 end
 
